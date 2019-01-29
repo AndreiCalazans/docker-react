f7a548ef0cdcfc47600f6438e6888d00ae245896
AndreiCalazans <andreixoc@hotmail.com>
2019-01-29 16:25:33 -0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,16 +8,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Learn docker</p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn nothing 2
           </a>
         </header>
       </div>
